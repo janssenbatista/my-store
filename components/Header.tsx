efe8f579f8d95998689ace0a8f9e06e6fa9924d5
Janssen Batista <batistajanssen.dev@gmail.com>
2024-08-09 08:11:07 -0300

    feat: update Header position

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,11 @@ import CartIcon from "./CartIcon";
 
 function Header() {
   return (
-    <header className={`bg-black flex justify-between items-center p-4`}>
-      <p className="text-3xl font-semibold text-white my-0">My Store</p>
-      <CartIcon />
+    <header className="bg-black p-3 sticky top-0 left-0 w-full">
+      <div className="flex items-center justify-between w-full">
+        <p className="text-3xl font-semibold text-white my-0">My Store</p>
+        <CartIcon />
+      </div>
     </header>
   );
 }
